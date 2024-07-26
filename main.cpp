a63656865ec629174fa38e097b2cf81d8bc15120
sheinasker <sheinasker@gmail.com>
2024-07-27 02:27:12 +0900

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+extern "C" char sample_code[];
+extern "C" std::uint32_t sample_code_size;
+
+int main() {
+    char* buf = (char*)std::malloc(sample_code_size);
+    std::memcpy(buf, sample_code, sample_code_size);
+    std::cout << "size: " << std::strlen(buf) << std::endl;
+    std::cout << "head: " << std::string(buf, buf + 10) << std::endl;
+    std::cout << "tail: " << std::string(buf + 8192 - 10, buf + 8192) << std::endl;
+}
