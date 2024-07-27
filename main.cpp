2457c3b90b1d9e7067c9c670e6b2dfb9e74b5422
sheinasker <sheinasker@gmail.com>
2024-07-27 15:20:13 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,5 +11,5 @@ int main() {
     std::memcpy(buf, sample_code, sample_code_size);
     std::cout << "size: " << std::strlen(buf) << std::endl;
     std::cout << "head: " << std::string(buf, buf + 10) << std::endl;
-    std::cout << "tail: " << std::string(buf + 8192 - 10, buf + 8192) << std::endl;
+    std::cout << "tail: " << std::string(buf + sample_code_size - 11, buf + sample_code_size - 1) << std::endl;
 }
