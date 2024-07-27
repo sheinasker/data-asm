477beae778b2f1ce3f443b9a77fde8e2955df7c0
sheinasker <sheinasker@gmail.com>
2024-07-27 15:20:53 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,10 @@ extern "C" std::uint32_t sample_code_size;
 int main() {
     std::cout << "address: " << reinterpret_cast<void*>(sample_code) << std::endl;
     char* buf = (char*)std::malloc(sample_code_size);
+
+    // SEGV
     std::memcpy(buf, sample_code, sample_code_size);
+    
     std::cout << "size: " << std::strlen(buf) << std::endl;
     std::cout << "head: " << std::string(buf, buf + 10) << std::endl;
     std::cout << "tail: " << std::string(buf + sample_code_size - 11, buf + sample_code_size - 1) << std::endl;
